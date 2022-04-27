f9d083cbcbdf5f76fca68ca5435773ea9de2369e
Ubaydullo <ubuhobbit@gmail.com>
2022-04-27 14:32:32 +0500

    simpe change

--- parse.py
+++ parse.py
@@ -9,24 +9,72 @@ class Parser:
     """
 
     def __init__(self, lexer):
-        pass
+        self.lexer = lexer
+
+        self.curToken = None
+        self.peekToken = None
+        self.nextToken()
+        self.nextToken() # Call this twice to initialize the current and peek tokens
 
     # return true if the current token matches
     def checkToken(self, kind):
-        pass
+        return kind == self.curToken.kind
 
     # return true if the next token matches
     def checkPeek(self, kind):
-        pass
+        return kind == self.peekToken.kind
 
     # try to match current token.
     # if not, error, Advances the current token
     def match(self, kind):
-        pass
+        if not self.checkToken(kind):
+            self.abort("Expected " + kind.name + ", got " + self.curToken.kind.name)
+        self.nextToken()
 
     # advances the current token
     def nextToken(self):
-        pass
+        self.curToken = self.peekToken
+        self.peekToken = self.lexer.getToken()
+        # no need to worry about passing the EOF, lexer handles that
 
     def abort(self, message):
         sys.exit("Error: " + message)
+
+    # Production rules.
+
+    # program ::= {statement}
+    def program(self):
+        print("PROGRAM")
+
+        # Parse all the statements in the program.
+        while not self.checkToken(TokenType.EOF):
+            self.statement()
+
+    # One of the following statements...
+    def statement(self):
+        # Check the first token to see what kind of statement this is.
+
+        # "PRINT" (expression | string)
+        if self.checkToken(TokenType.PRINT):
+            print("STATEMENT-PRINT")
+            self.nextToken()
+
+            if self.checkToken(TokenType.STRING):
+                # Simple string.
+                self.nextToken()
+            else:
+                # Expect an expression.
+                self.expression()
+
+        # Newline.
+        self.nl()
+    
+    # nl ::= '\n'+
+    def nl(self):
+        print("NEWLINE")
+		
+        # Require at least one newline.
+        self.match(TokenType.NEWLINE)
+        # But we will allow extra newlines too, of course.
+        while self.checkToken(TokenType.NEWLINE):
+            self.nextToken()
